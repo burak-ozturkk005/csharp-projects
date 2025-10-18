bafd07b07750526f8369f2f3dc2a30a3cba02d89
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-10-18 12:06:21 +0300

    numberAnalyzer

---
+++ numberAnalyzer/NumberAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberAnalyzer
+{
+    internal class NumberAnalyzer
+    {
+        public int Number { get; set; }
+        public NumberAnalyzer(int number)
+        {
+            Number = number;
+        }
+        public  string GetSign()
+        {
+            string word = "";
+                if (Number > 0)
+                {
+                return "Number is positive";
+                }
+                else if (Number < 0)
+                {
+                    return "Number is negative";
+                }
+                else
+                {
+                    return "Number is equal to zero";
+                }
+        }
+        public bool  isEven()
+        {
+            return Number % 2 == 0;
+        }
+        public bool  isPrime()
+        {
+            if (Number < 2)  return false;
+            for(int i = 2; i <= Math.Sqrt(Number);i++)
+            {
+                if( Number % i == 0)
+                {
+                    return false;
+                }else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+        public void isAnalyze()
+        {
+            Console.WriteLine($"Number is {Number}, {GetSign()}, Even: {(isEven() ? "Yes" : "No")}, Prime : {(isPrime() ? "Yes" : "No")}");
+        }
+    }
+}
---
+++ numberAnalyzer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace numberAnalyzer
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter a number to analyze: ");
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out int number))
+            {
+                NumberAnalyzer analyzer = new NumberAnalyzer(number);
+                analyzer.isAnalyze();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. please Enter a integer number.");
+            }
+          
+        }
+    }
+}
